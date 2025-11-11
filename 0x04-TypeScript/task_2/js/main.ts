fd35ea43fa14465033e8edb5b2bbf1e6aa911071
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2025-11-11 20:41:05 +0200

    5

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -38,3 +38,17 @@ function createEmployee(salary: number | string): Director | Teacher {
     return new Director();
   }
 }
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
+}
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
