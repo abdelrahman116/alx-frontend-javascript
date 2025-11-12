258ef424cc27efeef9d055fb6d695b1406c84972
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2025-11-12 11:17:59 +0200

    8

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -20,7 +20,7 @@ const row: RowElement = {
 
 const newRowID: RowID = CRUD.insertRow(row);
 
-const updateRow: RowElement = { ...row, age: 23 };
+const updatedRow: RowElement = { ...row, age: 23 };
 
-CRUD.updateRow(newRowID, updateRow);
+CRUD.updateRow(newRowID, updatedRow);
 CRUD.deleteRow(newRowID);
