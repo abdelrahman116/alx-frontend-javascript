33176d6c5a724519b78a1af94553e14288dc4057
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2025-11-12 11:15:10 +0200

    8

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -2,4 +2,4 @@ import { RowID, RowElement } from "./interface";
 
 declare function insertRow(row: RowElement): number;
 declare function updateRow(rowID: RowID): void;
-declare function deleteRow(rowID: RowID): void;
+declare function deleteRow(rowId: RowID): void;
